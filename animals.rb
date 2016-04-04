09b02647e4745ba87056430dfef2043cfd0e5243
goodbru <ryangoodhew@gmail.com>
2016-04-04 20:03:55 +0000

    first commit

---
+++ animals.rb
@@ -0,0 +1,27 @@
+class Animal
+end
+
+class Mammal
+end
+
+class Amphibian
+end
+
+class Primate
+end
+
+class Frog
+end
+
+class Bat 
+end
+
+class Bird
+end
+
+class Parrot
+end 
+
+class Chimpanzee
+end
+
